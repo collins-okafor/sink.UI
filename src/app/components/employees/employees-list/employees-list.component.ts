935f4aa1ba7367b7456399612e0de330618c4271
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-09-19 11:24:37 +0100

    seeding

--- src/app/components/employees/employees-list/employees-list.component.ts
+++ src/app/components/employees/employees-list/employees-list.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
 
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
-  styleUrls: ['./employees-list.component.css']
+  styleUrls: ['./employees-list.component.css'],
 })
-export class EmployeesListComponent {
+export class EmployeesListComponent implements OnInit {
+employees: Employee[] = [
+    {
+      id: '12',
+      name: 'John Doe',
+      email: 'jose.bush@gmail.com',
+      phone: 9329439843,
+      salary: 50000,
+      department: 'IT',
+    },
+    {
+      id: '23',
+      name: 'Sole Doe',
+      email: 'jose.bush@gmail.com',
+      phone: 9329439843,
+      salary: 50000,
+      department: 'IT',
+    },
+    {
+      id: '34',
+      name: 'Muze Doe',
+      email: 'jose.bush@gmail.com',
+      phone: 9329439843,
+      salary: 50000,
+      department: 'IT',
+    },
+    {
+      id: '45',
+      name: 'Num Doe',
+      email: 'jose.bush@gmail.com',
+      phone: 9329439843,
+      salary: 50000,
+      department: 'IT',
+    },
+    {
+      id: '56',
+      name: 'Xick Doe',
+      email: 'jose.bush@gmail.com',
+      phone: 9329439843,
+      salary: 50000,
+      department: 'IT',
+    },
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
+
+
